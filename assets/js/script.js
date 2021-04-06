0047504a10dde012b5fdea7befc26398becbe161
Dylan Cole <logboarddjc@gmail.com>
2021-04-06 13:33:08 -0400

    add saving each textarea to local storage and printing when page is reloaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,40 @@ $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
 
 let saveBttn = $(".save-icon");
-let textBox = $("textarea");
-let sibTextBox = saveBttn.siblings("textarea");
 let containerEl = $(".container");
-let textBox9am = $("#0");
-let textBox10am = $("#1");
+let schedule9am = $("#9AM");
+let schedule10am = $("#10AM");
+let schedule11am = $("#11AM");
+let schedule12pm = $("#12PM");
+let schedule1pm = $("#1PM");
+let schedule2pm = $("#2PM");
+let schedule3pm = $("#3PM");
+let schedule4pm = $("#4PM");
+let schedule5pm = $("#5PM");
+
+let scheduleArray = [];
+
+scheduleElArray = [
+    schedule9am,
+    schedule10am,
+    schedule11am,
+    schedule12pm,
+    schedule1pm,
+    schedule2pm,
+    schedule3pm,
+    schedule4pm,
+    schedule5pm,
+];
+
+renderLastRegistered();
 
 
 // TODO: make this function to render schedule
 function renderLastRegistered() {
+    for (i = 0; i < scheduleElArray.length; i++) {
+        scheduleArray.push(scheduleElArray[i].val(localStorage.getItem("time block " + i)));
 
+    }
 }
 
 // function for handling clicks
@@ -21,32 +45,35 @@ function handleFormSubmit(event) {
 
     let btnClicked = $(event.target);
 
+    let targetText = btnClicked.siblings("textarea");
  
-    console.log(btnClicked.siblings(textBox).val());
+    let targetTimeBlock = targetText.attr("data-position"); 
     
 
-    // TODO: finish this
-    let schedule = {
-        plan9am: textBox9am.val().trim(),
-        plan10am: textBox10am.val().trim(),
-    }
+    // 
+    scheduleArray = [
+        schedule9am.val(),
+        schedule10am.val(),
+        schedule11am.val(),
+        schedule12pm.val(),
+        schedule1pm.val(),
+        schedule2pm.val(),
+        schedule3pm.val(),
+        schedule4pm.val(),
+        schedule5pm.val(),
+    ];
 
-    schedule.plan9am = btnClicked.siblings(textBox).attr("id");
+    
 
-    // test to save text in object
-    console.log(schedule.plan9am + "here");
+// strings of the schedule for each hour saved in an array
 
 
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-
-    // if there is texted in textarea when save is clicked
-    if (btnClicked.siblings("textarea").val() !== "") {
-        // TODO: save textarea of sibs with clicked to object
-        // localStorage
-    }
+    localStorage.setItem("time block " +  targetTimeBlock, scheduleArray[targetTimeBlock]);
+    // localStorage.setItem("schedule", JSON.stringify(schedule));
 
 }
 
 saveBttn.on("click", handleFormSubmit);
+renderLastRegistered();
 
 // let time9am = moment()
